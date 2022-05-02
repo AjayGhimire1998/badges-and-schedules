02da15db4e9bb646cb5ec8e7efff7dcf5c4f1812
AjayGhimire <ajayghimire1998@gmail.com>
2022-05-02 17:21:03 +1000

    commit tree d4f9de59a468ea6bf0540e72869cd569fe71d0d0

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+    puts "Hello, my name is #{name}."
+end
+
+badge_maker "Arel"
+
+def batch_badge_creator array 
+    array.each {|name| badge_maker(name)}
+end
+
+names= ["ajay", "arjun", "sia"]
+batch_badge_creator names
